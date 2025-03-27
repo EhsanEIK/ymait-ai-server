a55565b0714a136e7626803757d64e5ec588b16c
EhsanEIK <ehsankhan1997.ek@gmail.com>
2025-03-27 10:49:10 +0600

    added DELETE method to delete the pc data from DB

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 // middleware
@@ -41,6 +41,15 @@ async function run() {
             const pcData = req.body;
             const result = await pcDataCollection.insertOne(pcData);
             res.send(result);
+        })
+
+        // DELETE: delete pc data from db
+        app.delete('/pcData/:id', async (req, res) => {
+            const pcData = req.params.id;
+            const query = { _id: new ObjectId(pcData) };
+            const result = await pcDataCollection.deleteOne(query);
+            res.send(result);
+
         })
 
         // Send a ping to confirm a successful connection
